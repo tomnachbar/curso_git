e150fcf002facbffefcdae56b757104697c3b33d
Tom Nachbar <nachbars@msn.com>
2024-01-04 16:22:14 -0300

    Adicionado funcao principal

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import streamlit as st
 def load_data():
         return pd.read_csv('data/processed/bikes_completed.csv')
 
-df = load_data()
+def main ():
+    df = load_data()
 
 st.dataframe(df)
+
+if __name__ == '__main__':
+      main()
